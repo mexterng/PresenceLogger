8014dd68fd67b28716c660baf77a12a18f293e58
Paul Meyer <okovessimon@yahoo.de>
2025-06-04 19:32:08 +0200

    fix bug to open new tab for edit page

--- static/scripts.js
+++ static/scripts.js
@@ -37,7 +37,7 @@ document.getElementById("groupSelect").addEventListener("change", () => {
                         <td><input type="checkbox" class="personCheck" data-index="${index}"></td>
                         <td>${person.lastname}</td>
                         <td>${person.firstname}</td>
-                        <td><a href="/edit?group=${encodeURIComponent(group)}&id=${person.id}" target="_blank" class="editIcon" title="Bearbeiten">&#9998;</a></td>
+                        <td><a href="/edit?group=${encodeURIComponent(group)}&id=${person.id}" class="editIcon" title="Bearbeiten">&#9998;</a></td>
                     </tr>`;
         });
         table += "</table>";
